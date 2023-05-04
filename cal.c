670a94ae440d184647cc68a430e0312e5747be34
wawa32565680 <81401540+wawa32565680@users.noreply.github.com>
2023-05-04 09:46:46 +0800

    Add files via upload

---
+++ cal.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include "calFun.h" //®Ö¤ß 
+
+int main(){
+	char str[MAX] = {'\0'};
+	int i;
+	double x;
+	gets(str);
+	printf("\n%g", count(str));
+}
